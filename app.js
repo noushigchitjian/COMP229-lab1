e0519639a6dc5d30c1598d84e096903c4efb9dcf
Noushig Chitjian <noushigchitjian@hotmail.com>
2020-09-18 20:34:51 -0400

    random questions created

---
+++ app.js
@@ -0,0 +1,51 @@
+const questionNumber = document.querySelector(".question-number");
+const questionText = document.querySelector(".question-text");
+const optionContainer = document.querySelector(".option-container");
+
+let questionCounter = 0;
+let currentQuestion;
+let availableQuestions = [];
+let availableOptions = [];
+
+
+function setAvailableQuestions(){
+    const totalQuestion = Test.length;
+    for (let i=0; i<totalQuestion; i++){
+        availableQuestions.push(Test[i])
+    }    
+}
+
+function getNewQuestion(){
+    questionNumber.innerHTML = "Question" + (questionCounter +1) + "of" + Test.length;
+    const questionIndex = availableQuestions[Math.floor(Math.random() * availableQuestions.length)]
+    currentQuestion = questionIndex;
+    questionText.innerHTML = currentQuestion.q;
+    const index1 = availableQuestions.indexOf(questionIndex);
+    availableQuestions.splice(index1,1);
+    const optionLen = currentQuestion.option.length
+   
+    for (let i=0; i<optionLen; i++){
+      availableOptions.push(i)  
+
+    }
+    for (let i = 0; i < optionLen; i++){
+        const option = document.createElement("div");
+        option.innerHTML = currentQuestion.options[i];
+        option.id = i;
+        option.className = "option";
+        optionContainer.appendChild(option)
+    }    
+    questionCounter++
+}
+function next(){
+    if(questionCounter === Test.length){
+        console.log("Test Over");
+    }
+    else{
+        getNewQuestion();
+    }
+}
+window.onload = function(){
+    setAvailableQuestions();
+    getNewQuestion();
+}
---
+++ questions.js
@@ -0,0 +1,57 @@
+console.log("Wellcome to your knowledge test")
+
+const Test = [
+    {
+        q: 'Which Month Are We in Today?',
+        options: ['March','June','September','December'],
+        answer: 2
+    },
+    {
+        q: 'What do red and yellow make?',
+        options: ['Red','Purple','Orange','None of the above'],
+        answer: 2
+    },
+    
+    {
+        q: 'How many keys are found on a normal keyboard?',
+        options: ['89','104','152','89'],
+        answer: 1
+    },
+    {
+        q: 'I like Basketball',
+        options: ['me too','i do not','that is not even a question','none of the above'],
+        answer: 2
+    },
+    {
+        q: 'How long is a normal narwhal tooth?',
+        options: ['1-3 feet','3-6 feet','6-9 feet','12-15 feet'],
+        answer: 2
+    },
+    {
+        q: 'What is the smallest country in the world?',
+        options: ['Canada','Brazil','Armenia','Vatican City'],
+        answer: 3
+    },
+
+    {
+        q: 'Why did the chicken cross the road?',
+        options: ['to get to the other side','to play with a friend','to find some food','none of the above'],
+        answer: 0
+    },
+    {
+        q: 'what is Superman real name?',
+        options: ['Jhon jhon','Clark Kent','Steven Clark','Kent joe'],
+        answer: 1
+    },
+    {
+        q: 'calculate the following and find the answer 9*9-25=',
+        options: ['81','49','56','43'],
+        answer: 2
+    },
+    {
+        q: 'There are 45 apples in your basket. You take three apples out of the basket. How many apples are left?',
+        options: ['3','45','42','41'],
+        answer: 1
+    },
+
+]
